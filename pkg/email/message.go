226518a98ae55733799129b8c4167a932cb1ef09
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-01-28 01:41:40 -0600

    docs

--- pkg/email/message.go
+++ pkg/email/message.go
@@ -13,16 +13,24 @@ type Message struct {
 	body []byte
 }
 
+// NewMessage builds a new basic email message from the given header and body.
 func NewMessage(h *Header, body []byte) *Message {
 	return &Message{*h, body}
 }
 
+// BodyString returns the message body as a string.
 func (m *Message) BodyString() string { return string(m.body) }
-func (m *Message) Body() []byte       { return []byte(m.BodyString()) }
 
-func (m *Message) SetBody(b []byte)       { m.body = b }
+// Body returns the message body.
+func (m *Message) Body() []byte { return []byte(m.BodyString()) }
+
+// SetBody sets the message body.
+func (m *Message) SetBody(b []byte) { m.body = b }
+
+// SetBodyString sets the message body from a string.
 func (m *Message) SetBodyString(s string) { m.body = []byte(s) }
 
+// String returns the email message as a string.
 func (m *Message) String() string {
 	var out strings.Builder
 	out.WriteString(m.Header.String())
