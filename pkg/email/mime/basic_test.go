9af777c8edcce2915983d59e194e3d2ffd0f63e1
Sterling Hanenkamp <sterling@hanenkamp.com>
2021-01-31 00:16:11 -0600

    beginning to add MIME tests

---
+++ pkg/email/mime/basic_test.go
@@ -0,0 +1,49 @@
+package mime
+
+import (
+	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func readTestFile(n string) []byte {
+	gopath := os.Getenv("GOPATH")
+	apppath := path.Join(gopath, "src/github.com/zostay/go-email")
+	tdpath := path.Join(apppath, "test")
+	tfpath := path.Join(tdpath, n)
+	data, _ := ioutil.ReadFile(tfpath)
+	return data
+}
+
+var (
+	mail1   = readTestFile("mail-1")
+	att1Gif = readTestFile("att-1.gif")
+)
+
+func TestBasic(t *testing.T) {
+	m, err := Parse(mail1)
+	assert.NoError(t, err)
+	assert.NotNil(t, m)
+
+	assert.NotNil(t, m.Parts)
+	assert.NotEmpty(t, m.Body())
+
+	bb, err := m.BodyBinary()
+	assert.NoError(t, err)
+
+	enc := base64.StdEncoding
+	binaryWant := make([]byte, enc.DecodedLen(len(m.Body())))
+	n, err := enc.Decode(binaryWant, m.Body())
+	assert.NoError(t, err)
+	binaryWant = binaryWant[:n]
+	assert.Equal(t, binaryWant, bb)
+
+	assert.Equal(t, "1.gif", m.Filename())
+	assert.Equal(t, att1Gif, bb)
+
+	assert.Equal(t, []string{"one", "two", "three"}, m.HeaderGetAll("X-MultiHeader"))
+}
