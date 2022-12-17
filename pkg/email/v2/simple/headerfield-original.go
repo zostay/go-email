308c094ba0b72bd9ed9c9ef3eb6d503a81a82a82
Sterling Hanenkamp <sterling@hanenkamp.com>
2022-12-17 12:03:07 -0600

    i decided header fields should not do line breaks

--- pkg/email/v2/simple/headerfield-original.go
+++ pkg/email/v2/simple/headerfield-original.go
@@ -28,17 +28,10 @@ var (
 type HeaderFieldOriginal struct {
 	field []byte // complete original field
 	colon int    // the index of the colon
-	lbr   int    // the start index of the break
 }
 
 // ParseOriginal will parse the given field and return a HeaderFieldOriginal. If
 // something goes wrong in parsing the field, it will return an error instead.
-// The input must include a complete header field with the last byte or two
-// being one of the email.Break values. The input may include multiple lines. It
-// will be parsed as a single header even if the fields are not properly folded.
-// There must be a colon the input.
-//
-// If any requirement for parsing is missing, an error will be returned.
 func ParseOriginal(field []byte) (*HeaderFieldOriginal, error) {
 	if len(field) < 3 {
 		return nil, ErrOriginalTooShort
@@ -50,28 +43,10 @@ func ParseOriginal(field []byte) (*HeaderFieldOriginal, error) {
 		return nil, ErrOriginalMissingColon
 	}
 
-	// the line break is either the ultimate or penultimate byte, start by
-	// checking the ultimate byte for line break
-	lbr := len(field) - 1
-	if field[lbr] != 0x0a && field[lbr] != 0x0d {
-		return nil, ErrOriginalMissingBreak
-	}
-
-	// now see if it starts at the penultimate byte
-	if field[lbr-1] == 0x0a || field[lbr-1] == 0x0d {
-		lbr--
-	}
-
 	return &HeaderFieldOriginal{
 		field: field,
-		lbr:   lbr,
 		colon: colon,
 	}, nil
-}
-
-// Break returns the line break used to terminate this header field.
-func (f *HeaderFieldOriginal) Break() email.Break {
-	return email.Break(f.field[f.lbr:])
 }
 
 // String returns the original as a string.
@@ -95,5 +70,4 @@ func (f *HeaderFieldOriginal) Body() string {
 }
 
 var _ email.HeaderField = &HeaderFieldOriginal{}
-var _ email.WithBreak = &HeaderFieldOriginal{}
 var _ email.Outputter = &HeaderFieldOriginal{}
