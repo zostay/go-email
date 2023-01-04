b97e315477d3135aac1ce8ce758b17345d7d0ffe
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-01-03 23:43:18 -0600

    i preferred b-encoding before, so let's do that

--- pkg/v2/header/field/transcode.go
+++ pkg/v2/header/field/transcode.go
@@ -7,10 +7,10 @@ import (
 
 // Encode transforms a single header field body by looking for any characters
 // allowed for header encoding and turning them into encode body values using
-// word encoder. It will always output q-type encoding using UTF-8 as the
-// character set.
+// word encoder. It will always output b-type (Base-64) encoding using UTF-8 as
+// the character set.
 func Encode(body string) string {
-	return mime.QEncoding.Encode("UTF-8", body)
+	return mime.BEncoding.Encode("UTF-8", body)
 }
 
 // Decode transforms a single header field body and looks for MIME word encoded field
