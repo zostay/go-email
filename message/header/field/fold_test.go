105d7763527317a54bb09335b1d7604696a67577
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-01-18 19:59:34 -0600

    fix folding test to deal with roundtrip fixes

--- message/header/field/fold_test.go
+++ message/header/field/fold_test.go
@@ -40,7 +40,7 @@ DomainKey-Signature: a=rsa-sha1; c=nofws; q=dns; s=k1; d=mail7.example.com;
 Received: from (127.0.0.1) by mail7.example.com id asdfasdfasdf for <sterling@example.com>; Sat, 31 Jan 2015 03:23:09 +0000 (envelope-from <bounce-mc.us2_6.1-sterling=example.com@mail7.example.com>)
 Subject: =?utf-8?Q?Emulator=20Behind=20The=20Scenes=2C=20Debugging=20Guides=2C=20New=20Meetup=20Groups=20and=20more=21?=
 From: =?utf-8?Q?Example?= <devsupport@example.com>
-Reply-To: =?utf-8?Q?Example?= <devsupport@gexample.com>
+Reply-To: =?utf-8?Q?Example?= <devsupport@example.com>
 To: <sterling@example.com>
 Date: Sat, 31 Jan 2015 03:23:09 +0000
 Message-ID: <asdfasdfasdfasdfasdfasdfasdfasdfasd.2@mail7.example.com>
@@ -69,42 +69,83 @@ Hello.
 --_----------=_MCPart_433295335--
 `
 
-const emailMsgFolded = `Delivered-To: sterling@example.com
-Received: by 1.6.2.1 with SMTP id asdfasdfasdfasd;
-        Fri, 30 Jan 2015 19:23:13 -0800 (PST)
-X-Received: by 1.2.8.2 with SMTP id asdfasdfasdfasd.5.3;
-        Fri, 30 Jan 2015 19:23:12 -0800 (PST)
+const emailMsgUnfolded = `Delivered-To: sterling@example.com
+Received: by 1.6.2.1 with SMTP id asdfasdfasdfasd;        Fri, 30 Jan 2015 19:23:13 -0800 (PST)
+X-Received: by 1.2.8.2 with SMTP id asdfasdfasdfasd.5.3;        Fri, 30 Jan 2015 19:23:12 -0800 (PST)
 Return-Path: <bounce-mc.us2_6.1-sterling=example.com@mail7.example.com>
-Received: from mail7.example.com (mail7.example.com. [1.2.1.7])
-        by mx.example.com with ESMTP id asdfasdfasdfasdf.1.2.0.3.1.2.1
-        for <sterling@example.com>;
-        Fri, 30 Jan 2015 19:23:12 -0800 (PST)
+Received: from mail7.example.com (mail7.example.com. [1.2.1.7])        by mx.example.com with ESMTP id asdfasdfasdfasdf.1.2.0.3.1.2.1        for <sterling@example.com>;        Fri, 30 Jan 2015 19:23:12 -0800 (PST)
+Received-SPF: pass (example.com: domain of bounce-mc.us2_6.1-sterling=example.com@mail7.example.com designates 1.2.1.7 as permitted sender) client-ip=1.2.1.7;
+Authentication-Results: mx.example.com;       spf=pass (example.com: domain of bounce-mc.us2_6.1-sterling=example.com@mail7.example.com designates 1.2.1.7 as permitted sender) smtp.mail=bounce-mc.us2_6.1-sterling=example.com@mail7.example.com;       dkim=pass header.i=@mail7.example.com
+DKIM-Signature: v=1; a=rsa-sha1; c=relaxed/relaxed; s=k1; d=mail7.example.com; h=Subject:From:Reply-To:To:Date:Message-ID:List-ID:List-Unsubscribe:Sender:Content-Type:MIME-Version; i=devsupport=3Dexample.com@mail7.example.com; bh=asdfasdfasdfasdfasdfasdfasdf; b=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf   asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf   asdfasdfasdfasdfasdf
+DomainKey-Signature: a=rsa-sha1; c=nofws; q=dns; s=k1; d=mail7.example.com; b=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf   asdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf   asdfasdfasdfasdfasdf;
+Received: from (127.0.0.1) by mail7.example.com id asdfasdfasdf for <sterling@example.com>; Sat, 31 Jan 2015 03:23:09 +0000 (envelope-from <bounce-mc.us2_6.1-sterling=example.com@mail7.example.com>)
+Subject: Emulator Behind The Scenes, Debugging Guides, New Meetup Groups and more!
+From: Example <devsupport@example.com>
+Reply-To: Example <devsupport@example.com>
+To: <sterling@example.com>
+Date: Sat, 31 Jan 2015 03:23:09 +0000
+Message-ID: <asdfasdfasdfasdfasdfasdfasdfasdfasd.2@mail7.example.com>
+X-Mailer: MailChimp Mailer - **asdfasdfasdfasdfasdfasd**
+X-Campaign: mailchimpasdfasdfasdfasdfasdfasdfa.asdfasdfas
+X-campaignid: mailchimpasfdasdfasdfasdfasdfasdfa.asdfasdfas
+X-Report-Abuse: Please report abuse for this campaign here: http://www.example.com/abuse/abuse.phtml?u=asdfasdfasdfasdfasdfasdfa&id=asdfasdfas&e=asdfasdfas
+X-MC-User: asdfasdfasdfasdfasdfasdfa
+X-Feedback-ID: 6:6.1:us2:mc
+List-ID: asdfasdfasdfasdfasdfasdfasd list <asdfasdfasdfasdfasdfasdfa.7.list-id.example.com>
+X-Accounttype: pd
+List-Unsubscribe: <mailto:unsubscribe-asdfasdfasdfasdfasdfasdfa-asdfasdfas-asdfasdfas@mailin1.example.com?subject=unsubscribe>, <http://example.us2.example.com/unsubscribe?u=asdfasdfasdfasdfasdfasdfa&id=asdfasdfas&e=asdfasdfas&c=asdfasdfas>
+Sender: "Example" <devsupport=example.com@mail7.example.com>
+x-mcda: FALSE
+Content-Type: multipart/alternative; boundary="_----------=_MCPart_433295335"
+MIME-Version: 1.0
+Keywords: 
+
+This is a multi-part message in MIME format
+
+--_----------=_MCPart_433295335
+Content-Type: text/plain; charset="utf-8"; format="fixed"
+Content-Transfer-Encoding: quoted-printable
+
+Hello.
+--_----------=_MCPart_433295335--
+`
+
+const emailMsgFolded = `Delivered-To: sterling@example.com
+Received: by 1.6.2.1 with SMTP id asdfasdfasdfasd;        Fri, 30 Jan 2015
+ 19:23:13 -0800 (PST)
+X-Received: by 1.2.8.2 with SMTP id asdfasdfasdfasd.5.3;        Fri, 30 Jan
+ 2015 19:23:12 -0800 (PST)
+Return-Path: <bounce-mc.us2_6.1-sterling=example.com@mail7.example.com>
+Received: from mail7.example.com (mail7.example.com. [1.2.1.7])        by
+ mx.example.com with ESMTP id asdfasdfasdfasdf.1.2.0.3.1.2.1        for
+ <sterling@example.com>;        Fri, 30 Jan 2015 19:23:12 -0800 (PST)
 Received-SPF: pass (example.com: domain of
  bounce-mc.us2_6.1-sterling=example.com@mail7.example.com designates 1.2.1.7
  as permitted sender) client-ip=1.2.1.7;
-Authentication-Results: mx.example.com;
-       spf=pass (example.com: domain of
- bounce-mc.us2_6.1-sterling=example.com@mail7.example.com designates 1.2.1.7
- as permitted sender)
- smtp.mail=bounce-mc.us2_6.1-sterling=example.com@mail7.example.com;
-       dkim=pass header.i=@mail7.example.com
-DKIM-Signature: v=1; a=rsa-sha1; c=relaxed/relaxed; s=k1; d=mail7.example.com;
+Authentication-Results: mx.example.com;       spf=pass (example.com: domain
+ of bounce-mc.us2_6.1-sterling=example.com@mail7.example.com designates
+ 1.2.1.7 as permitted sender)
+ smtp.mail=bounce-mc.us2_6.1-sterling=example.com@mail7.example.com;      
+ dkim=pass header.i=@mail7.example.com
+DKIM-Signature: v=1; a=rsa-sha1; c=relaxed/relaxed; s=k1;
+ d=mail7.example.com;
  h=Subject:From:Reply-To:To:Date:Message-ID:List-ID:List-Unsubscribe:Sender:Content-Type:MIME-Version;
  i=devsupport=3Dexample.com@mail7.example.com;
  bh=asdfasdfasdfasdfasdfasdfasdf;
  b=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
-   asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
-   asdfasdfasdfasdfasdf
+ asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf 
+ asdfasdfasdfasdfasdf
 DomainKey-Signature: a=rsa-sha1; c=nofws; q=dns; s=k1; d=mail7.example.com;
  b=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
-   asdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
-   asdfasdfasdfasdfasdf;
+ asdfasfdasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf 
+ asdfasdfasdfasdfasdf;
 Received: from (127.0.0.1) by mail7.example.com id asdfasdfasdf for
  <sterling@example.com>; Sat, 31 Jan 2015 03:23:09 +0000 (envelope-from
  <bounce-mc.us2_6.1-sterling=example.com@mail7.example.com>)
-Subject: =?utf-8?Q?Emulator=20Behind=20The=20Scenes=2C=20Debugging=20Guides=2C=20New=20Meetup=20Groups=20and=20more=21?=
-From: =?utf-8?Q?Example?= <devsupport@example.com>
-Reply-To: =?utf-8?Q?Example?= <devsupport@gexample.com>
+Subject: Emulator Behind The Scenes, Debugging Guides, New Meetup Groups and
+ more!
+From: Example <devsupport@example.com>
+Reply-To: Example <devsupport@example.com>
 To: <sterling@example.com>
 Date: Sat, 31 Jan 2015 03:23:09 +0000
 Message-ID: <asdfasdfasdfasdfasdfasdfasdfasdfasd.2@mail7.example.com>
@@ -124,7 +165,7 @@ Sender: "Example" <devsupport=example.com@mail7.example.com>
 x-mcda: FALSE
 Content-Type: multipart/alternative; boundary="_----------=_MCPart_433295335"
 MIME-Version: 1.0
-Keywords:
+Keywords: 
 
 This is a multi-part message in MIME format
 
@@ -136,12 +177,21 @@ Hello.
 --_----------=_MCPart_433295335--
 `
 
+func clearRawFields(h *header.Header) {
+	for _, f := range h.ListFields() {
+		f.Raw = nil
+	}
+}
+
 func TestMessageFoldIntegration(t *testing.T) {
 	t.Parallel()
 
 	m, err := message.Parse(strings.NewReader(emailMsg), message.WithoutMultipart())
 	assert.NoError(t, err)
 	require.NotNil(t, m)
+
+	// folding is disabled when Raw is present
+	clearRawFields(m.GetHeader())
 
 	// despite parsing, we want folding
 	m.GetHeader().SetFoldEncoding(field.DefaultFoldEncoding)
@@ -164,6 +214,9 @@ func TestMessageDoNotFoldEncodingIntegration(t *testing.T) {
 	assert.NoError(t, err)
 	require.NotNil(t, m)
 
+	// folding is disabled when Raw is present
+	clearRawFields(m.GetHeader())
+
 	assert.Equal(t, len(m.GetHeader().ListFields()), 30)
 
 	from, err := m.GetHeader().Get(header.From)
@@ -172,7 +225,7 @@ func TestMessageDoNotFoldEncodingIntegration(t *testing.T) {
 
 	s := &bytes.Buffer{}
 	_, _ = m.WriteTo(s)
-	assert.Equal(t, emailMsg, s.String())
+	assert.Equal(t, emailMsgUnfolded, s.String())
 }
 
 func TestNewFoldEncoding(t *testing.T) {
