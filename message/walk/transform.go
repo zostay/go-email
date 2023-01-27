b046ce27165d6beb825d697ec2e370832563a301
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-01-27 15:44:15 -0600

    complete draft of AndTransform

--- message/walk/transform.go
+++ message/walk/transform.go
@@ -1,26 +1,10 @@
 package walk
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
 	"io"
 
 	"github.com/zostay/go-email/v2/message"
-)
-
-var (
-	// ErrSkip may be returned by a Transformer callback to signal that the part
-	// should be skipped entirely.
-	ErrSkip = errors.New("skip part")
-
-	// ErrCopy may be returned by a Transformer callback to signal that the part
-	// should be copied as is.
-	ErrCopy = errors.New("clone part")
-
-	// ErrNilNil is returned by AndTransform when a Transformer callback returns
-	// no parts and provides no error.
-	ErrNilNil = errors.New("no parts and no error")
 )
 
 // BadTransformationError is used when transformation needs to fail with an
@@ -43,23 +27,41 @@ func (b *BadTransformationError) Unwrap() error {
 // Transformer is a callback that can be passed to the AndTransform() function
 // to transform a message and its sub-parts into a new message.
 //
-// The Transformer is given the part to transform and the ancestry of the part.
-// If len(parents) is zero, then this is the top-level part. The parents are the
-// original parents of the given original part, not the transformed parents.
+// The Transformer is given a part to transform and the ancestry of the part. If
+// len(parents) is zero, then this is the top-level part (i.e., the top-level
+// part that AndTransform() was called upon, which might not be root message).
+// The parents are the original parents of the given original part, not the
+// transformed parents. The Transformer should only work to transform the part
+// given and not it's children. It will be called again to transform those after
+// it transforms the parent.
 //
-// The transform must return zero or more message.Buffer objects or an
-// error. If message.Buffer objects are returned, each object must return a
+// The Transformer function must return zero or more message.Buffer objects or
+// an error.
+//
+// When message.Buffer objects are returned, each object must return a
 // message.BufferMode other than message.ModeUnset (i.e., either
-// message.ModeOpaque or message.ModeMultipart).
+// message.ModeOpaque or message.ModeMultipart). If the message.BufferMode of a
+// returned message.Buffer is message.ModeMultipart, it may be empty or have
+// parts already present but those parts must also be message.Buffer objects and
+// so on down the tree, or you may have unexpected results.
+//
+// If nothing is returned, transformation of children will continue on anyway.
+// Any transformed children will be attached to the nearest transformed
+// parent(s). If there is no such parent, they will be returned from
+// AndTransform(). IF multiple message.Buffer objects are returned, then any
+// children transformed will be attached to all of them that have a
+// message.BufferMode of message.ModeMultipart.
 //
 // If an error is returned, it will result in AndTransform() failing with that
 // error.
-type Transformer func(part message.Part, parents []message.Part) ([]message.Buffer, error)
+type Transformer func(part message.Part, parents []message.Part) ([]*message.Buffer, error)
 
-// AndTransform will perform a transformation on the given message. This will
-// process the message as is (i.e., if there's a message.Opaque part whose bytes
-// describe sub-parts, that message.Opaque part will be processed was as a
-// single item). The transformation is performed in depth-first order.
+// AndTransform will perform a transformation on the given message or message
+// part. This will process the message as is (i.e., if there's a message.Opaque
+// part whose bytes describe sub-parts, that message.Opaque part will be
+// processed was as a single item). The transformation is performed in
+// depth-first order. Each part in the message three will be transformed exactly
+// once.
 //
 // The parents will be transformed before the children. If a multipart message
 // part is transformed into an opaque part, then its children will not be
@@ -84,88 +86,61 @@ type Transformer func(part message.Part, parents []message.Part) ([]message.Buff
 func AndTransform(
 	transformer Transformer,
 	msg message.Part,
-) ([]message.Part, error) {
+) ([]*message.Buffer, error) {
 	parents := make([]message.Part, 0, 10)
 	return andTransform(transformer, msg, parents)
 }
 
 func andTransform(
 	transformer Transformer,
-	msg message.Part,
+	part message.Part,
 	parents []message.Part,
-) ([]message.Part, error) {
-	switch msg.IsMultipart() {
-	case true:
-		tbufs, err := transformer(msg, parents)
-		if err != nil {
-			return nil, err
-		}
+) ([]*message.Buffer, error) {
+	tParts, err := transformer(part, parents)
+	if err != nil {
+		return nil, err
+	}
 
-		hasMultipart := false
-		for _, tbuf := range tbufs {
-			hasMultipart = hasMultipart || tbuf.Mode() == message.ModeMultipart
-			if hasMultipart {
-				break
+	if part.IsMultipart() {
+		allTSubParts := make([]*message.Buffer, 0, len(part.GetParts()))
+		parents = append(parents, part)
+		for _, subPart := range part.GetParts() {
+			tSubParts, err := andTransform(transformer, subPart, parents)
+			if err != nil {
+				return nil, err
 			}
+
+			allTSubParts = append(allTSubParts, tSubParts...)
 		}
 
-		if hasMultipart {
-
+		if len(tParts) > 0 {
+			for _, tPart := range tParts {
+				tPart.AddBuffers(allTSubParts...)
+			}
+		} else {
+			return allTSubParts, nil
 		}
 	}
-	return nil, fmt.Errorf("not implemented")
+
+	return tParts, nil
 }
 
-// func handleTransformationErrors(
-// 	origPart message.Part,
-// 	newParts []message.Part,
-// 	err error,
-// ) ([]message.Part, error) {
-// 	var bte *BadTransformationError
-// 	if newParts == nil && err == nil {
-// 		return nil, &BadTransformationError{ErrNilNil, "Transformer error"}
-// 	} else if newParts != nil && err == != nil {
-// 		return nil, &BadTransformationError{err, "Transformer incorrectly returned error and parts"}
-// 	} else if newParts != nil {
-// 		return newParts, nil
-// 	} else if errors.Is(err, ErrSkip) {
-// 		return nil, ErrSkip
-// 	} else if errors.Is(err, ErrCopy) {
-// 		return copyPart(origPart)
-// 	} else {
-// 		return nil, err
-// 	}
-// }
+// PartToBuffer is a tool similar to message.BufferFrom(), with two
+// important differences. First, the return value matches the expected return
+// value of Transformer. Second, the message.BufferFrom() function copies an
+// object and all of its children recursively. This does not copy andy children
+// and only deals with the part itself if it's a multipart part.
+func PartToBuffer(orig message.Part) ([]*message.Buffer, error) {
+	cp := []*message.Buffer{
+		{Header: *orig.GetHeader().Clone()},
+	}
 
-// TransCopyPart provides a handy utility for copying an original part through
-// to make a transformed part with no changes. This is intended for use with
-// defining a Transformer, so this doesn't exactly copy a part.
-//
-// A non-multipart part will be copied into message.Opaque by cloning its
-// headers and then copying its io.Reader into a buffer attached to the returned
-// part.
-//
-// A multipart part will result in the creation of a new message.Multipart with
-// headers cloned from the original. But it will be empty.
-func TransCopyPart(orig message.Part) (message.Generic, error) {
-	var copy message.Generic
-	header := orig.GetHeader().Clone()
-	if orig.IsMultipart() {
-		copy = &message.Multipart{
-			Header: *header,
-		}
-	} else {
-		buf := &bytes.Buffer{}
-		_, err := io.Copy(buf, orig.GetReader())
+	if !orig.IsMultipart() {
+		_, err := io.Copy(cp[0], orig.GetReader())
 		if err != nil {
 			return nil, err
 		}
-
-		copy = &message.Opaque{
-			Header: *header,
-			Reader: buf,
-		}
 	}
 
-	return copy, nil
+	return cp, nil
 }
