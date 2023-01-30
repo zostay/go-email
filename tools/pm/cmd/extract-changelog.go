9238908931a7471143c0a4ac5753f5b29d5fc186
Sterling Hanenkamp <sterling@hanenkamp.com>
2023-01-30 17:09:30 -0600

    fix how pm commannds find Changes.md

--- tools/pm/cmd/extract-changelog.go
+++ tools/pm/cmd/extract-changelog.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/zostay/go-email/v2/tools/pm/changes"
+	"github.com/zostay/go-email/v2/tools/pm/release"
 )
 
 var (
@@ -20,7 +21,7 @@ var (
 )
 
 func ExtractChangelog(_ *cobra.Command, args []string) {
-	r, err := changes.ExtractSection("Changes", args[0])
+	r, err := changes.ExtractSection(release.GoEmailConfig.Changelog, args[0])
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Failed to read changelog section: %v\n", err)
 		os.Exit(1)
